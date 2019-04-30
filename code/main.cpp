c6b79ff2190814d7e1b17097b888c9dea9df82d4
xarvie <ftp@outlook.com>
2019-04-30 08:43:35 +0800

    add module

---
+++ code/main.cpp
@@ -0,0 +1,27 @@
+#include <Poller.h>
+
+
+class Server :public Poller{
+public:
+    explicit Server(int port, int maxWorker = 4): Poller(port, maxWorker)
+    {
+
+    }
+    int onReadMsg(Session &conn, int bytesNum) override
+    {
+        Msg msg;
+        msg.buff = conn.readBuffer.buff;
+        msg.len = conn.readBuffer.size;
+
+        this->sendMsg(conn, msg);
+
+        return bytesNum;
+    }
+};
+int main()
+{
+    Server sx(9876, 4);
+    sx.run();
+
+    return 0;
+}
